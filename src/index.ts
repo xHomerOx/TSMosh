1f9ad0a6b61e071ca57e5566c26bbe0fb3e0038e
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-08-16 16:03:24 -0300

    Any / Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-let age: number = 20;
+/* Any Type */
 
-if (age < 50) age += 10;
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;
+
+function render(document: any) {
+    console.log(document);
+}
+
+/* Arrays */
+
+// let numbers: number[] = [1, 2, 3];
+let numbers: number[] = [];
+// numbers[0] = 1;
+// numbers[1] = '1';
+
+numbers.forEach(n => n.toFixed);
+
+/* Tuples */
+
