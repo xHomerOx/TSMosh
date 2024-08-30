b6239ac81c7e3a0fe32afc149caf10b81c78fbd2
OTELLO BOZ MARALDO <oboz@agn.gov.ar>
2024-08-30 12:22:25 -0300

    Read Only Properties

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,31 @@ console.log(account instanceof Account);
 
 //Union
 // if (typeof someObj === 'number') {}
+
+/* Read-only and Optional Properties */
+
+class Account_2 {
+    readonly id: number;
+    owner: string;
+    balance: number;
+    nickname?: string;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0) {
+            throw new Error("Invalid amount");
+        }else{
+            this.balance += amount;
+        }
+    }
+}
+
+let account_2 = new Account(1, 'Otello', 0);
+
+/* Access Control Keywords */
+
