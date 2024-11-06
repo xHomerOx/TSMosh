9b2c657c1a42c30ee1c2ef1d75dd7232a9de3bb0
Otello Boz Maraldo <homero.tw@gmail.com>
2024-11-05 21:18:42 -0300

    Fundamentals

--- src/Fundamentals/index.ts
+++ src/Fundamentals/index.ts
@@ -1,4 +1,5 @@
 // For each of these values, what type will the TypeScript compiler infer? 
+
 let a: number = 100; 
 let b: string = 'Coffee'; 
 let c: boolean[] = [true, false, false]; 
@@ -8,6 +9,7 @@ let f: void;
 let g: [] = [];
 
 // What are the compilation errors in each of the following code snippets?
+
 let song: { title: string, releaseYear: number} = { title: 'My song', releaseYear: 2024};
 let prices: any[] = [100, 200, 300];
 prices[0] = '$100'; 
