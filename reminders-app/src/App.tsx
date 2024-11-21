a316009818bc512af4611b8fe96765574c82d8d4
Otello Boz Maraldo <homero.tw@gmail.com>
2024-11-20 23:11:43 -0300

    Calling the Backend TS

--- reminders-app/src/App.tsx
+++ reminders-app/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    {id: 1, title: 'Reminder 1'}
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ReminderList items={reminders} />
     </div>
   );
 }
